2645aecb49cfe8145056c24aa1d85cd75a00326d
Дмитро Лебедь <ghdater96@gmail.com>
2026-05-01 18:19:07 +0300

    Laba 5

--- labs/views.py
+++ labs/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 def home(request):
+    category_id = request.GET.get('category')
+    query = request.GET.get('q')
+
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
     context = {
-        'title': 'Головна сторінка',
-        'message': 'Це головна сторінка'
+        'products': products,
+        'categories': categories
     }
-    return render(request, 'labs/template.html', context)
 
-
-def page1(request):
-    context = {
-        'title': 'Сторінка 1',
-        'message': 'Це перша сторінка'
-    }
-    return render(request, 'labs/template.html', context)
-
-
-def page2(request):
-    context = {
-        'title': 'Сторінка 2',
-        'message': 'Це друга сторінка'
-    }
-    return render(request, 'labs/template.html', context)
+    return render(request, 'labs/home.html', context)
